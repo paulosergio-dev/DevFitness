42e8e460d0d67ea7836f80c7f168ff4b64b6aa9b
paulosergio-dev <paulosergioo-@hotmail.com>
2021-06-03 22:09:03 -0300

    DevFitness

---
+++ DevFitness.ConsoleApp/Comida.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFitness.ConsoleApp
+{
+    public class Comida : Refeicao
+    {
+        public Comida(string descricao, int calorias, decimal preco) : base(descricao, calorias)
+        {
+            Preco = Preco;
+        }
+
+        public decimal Preco { get; private set; }
+
+        public override void Imprimiren{ get; set; }
+    }
+}
---
+++ DevFitness.ConsoleApp/Refeicao.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFitness.ConsoleApp
+{
+    public class Refeicao
+    {
+        public Refeicao(string descricao, int calorias)
+        {
+            Descricao = descricao;
+            Calorias = calorias;
+        }
+
+        public string Descricao { get; private set; }
+        public int Calorias { get; private set; }
+
+        public virtual void ImprimirMensagem()
+        {
+            Console.WriteLine($"{Descricao}, com {Calorias} calorias.");
+        }
+    }
+}
